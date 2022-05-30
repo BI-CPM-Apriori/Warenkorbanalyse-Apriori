96b897f1b87a413bd8a7283f9c77d94412dbff7e
danielkipp <daniel.kipp@student.reutlingen-university.de>
2022-05-30 13:38:00 +0200

    Update dbConnection.py

--- Database/dbConnection.py
+++ Database/dbConnection.py
@@ -3,7 +3,7 @@ import pyodbc
 def openConn():
 
     # lokales Servername ('[Rechnername]\SQLEXPRESS') für MS SQL Server Express
-    rechner_name = ''
+    rechner_name = 'NB-DK-DELL'
     server_name = rechner_name + '\SQLEXPRESS' 
     db_name = 'AdventureWorks2019' 
     connection_str =  'Driver={SQL Server};' + 'Server=' + str(server_name) + ";Database=" + str(db_name) + ";Trusted_Connection=yes;"
